9112afbd87dd4111751c9c57aec31fc6c62a6c50
BarryHaworth <barryhaworth@gmail.com>
2022-06-30 19:59:12 +1000

    Working on plots

--- pg_plots.R
+++ pg_plots.R
@@ -2,6 +2,7 @@
 # Plot the average value of Parental Guide scores over time.
 # For each MPAA rating (PG, PG-13, R) look at trend in Parental Guides over time
 #  This is a first go - still needs some work.
+#  Still needs some work.
 
 library(dplyr)
 library(ggplot2)
@@ -9,42 +10,57 @@ library(forcats)
 
 PROJECT_DIR <- "c:/R/parental"
 DATA_DIR    <- paste0(PROJECT_DIR,"/data")
+PLOT_DIR    <- paste0(PROJECT_DIR,"/plot")
 
 load(paste0(DATA_DIR,"/parental_guide.RData"))
+load(paste0(DATA_DIR,"/country_certificate.RData"))
 
 names(parental_guide)
-table(parental_guide$mpaa_rating)
+names(country_certificate)
+
+table(country_certificate$country)
+
+cc_table <- data.frame(table(country_certificate$country)) 
+names(cc_table) <- c("country","freq")
 
 guides <- c("sex","violence","profanity","drugs","intense")  # List of Parental Guide variables
 
-parental_guide$sex <- factor(parental_guide$sex,levels=c("None","Mild","Moderate","Severe"))
-parental_guide$violence <- factor(parental_guide$violence,levels=c("None","Mild","Moderate","Severe"))
+parental_guide$sex       <- factor(parental_guide$sex,levels=c("None","Mild","Moderate","Severe"))
+parental_guide$violence  <- factor(parental_guide$violence,levels=c("None","Mild","Moderate","Severe"))
 parental_guide$profanity <- factor(parental_guide$profanity,levels=c("None","Mild","Moderate","Severe"))
-parental_guide$drugs <- factor(parental_guide$drugs,levels=c("None","Mild","Moderate","Severe"))
-parental_guide$intense <- factor(parental_guide$intense,levels=c("None","Mild","Moderate","Severe"))
+parental_guide$drugs     <- factor(parental_guide$drugs,levels=c("None","Mild","Moderate","Severe"))
+parental_guide$intense   <- factor(parental_guide$intense,levels=c("None","Mild","Moderate","Severe"))
 
 summary(parental_guide %>% select(sex,violence,profanity,drugs,intense))
 
-# Keep the known ratings
-ratings =c("PG","PG-13","R")
-parental_guide <- parental_guide %>% filter(mpaa_rating %in% ratings) %>% filter(startYear > 1989) 
+# United States ratings
 
-ggplot(data=parental_guide) +
+mpaa <- c("G","PG","PG-13","R","X","NC-17")  # MPAA ratings
+
+cc_us <- country_certificate %>% filter(country=="United States") %>% filter(certificate %in% mpaa)
+
+pg_us <- parental_guide %>% select(-certificate) %>% inner_join(cc_us,by="tconst")
+
+data.frame(table(pg_us$certificate)) 
+
+ggplot(data=pg_us) +
   geom_histogram((aes(x=startYear)),  fill="cornflowerblue" ,binwidth = 1)
 
-ggplot(data=parental_guide, aes(x=startYear, fill=mpaa_rating)) +
+ggplot(data=pg_us %>% filter(startYear>= 1970), aes(x=startYear, fill=certificate)) +
   geom_bar()
 
-ggplot(data=parental_guide %>% filter(mpaa_rating=="PG",sex !=""), aes(x=startYear, fill=fct_rev(sex) )) +
+ggplot(data=pg_us %>% filter(startYear>= 1970, certificate=="G",sex !=""), aes(x=startYear, fill=fct_rev(sex) )) +
   geom_bar(position="fill")
 
-ggplot(data=parental_guide %>% filter(mpaa_rating=="PG-13",sex !=""), aes(x=startYear, fill=fct_rev(sex) )) +
+ggplot(data=pg_us %>% filter(startYear>= 1970, certificate=="PG",sex !=""), aes(x=startYear, fill=fct_rev(sex) )) +
   geom_bar(position="fill")
 
-ggplot(data=parental_guide %>% filter(mpaa_rating=="R",sex !=""), aes(x=startYear, fill=fct_rev(sex) )) +
+ggplot(data=pg_us %>% filter(startYear>= 1970, certificate=="PG-13",sex !=""), aes(x=startYear, fill=fct_rev(sex) )) +
   geom_bar(position="fill")
 
-ggplot(data=parental_guide %>% filter(mpaa_rating=="PG",violence !=""), aes(x=startYear, fill=fct_rev(violence))) +
+ggplot(data=pg_us %>% filter(startYear>= 1970, certificate=="R",sex !=""), aes(x=startYear, fill=fct_rev(sex) )) +
   geom_bar(position="fill")
+
+
 
 
