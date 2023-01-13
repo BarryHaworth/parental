3eab4969f77acc44ab96513965978d62f7ed2042
BarryHaworth <barryhaworth@gmail.com>
2023-01-13 21:16:30 +1000

    Added Stacked Area charts with overlayed Average line.  Still has a glitch.

--- pg_plot_us.R
+++ pg_plot_us.R
@@ -2,11 +2,14 @@
 # Plot the average value of Parental Guide scores over time.
 # For each MPAA rating (PG, PG-13, R) look at trend in Parental Guides over time
 #
+# Changed file format to SVG instead of PNG
 
 library(dplyr)
 library(ggplot2)
 library(forcats)
 library(reshape2)
+library(stringr)
+library(forcats)
 
 PROJECT_DIR <- "c:/R/parental/"
 DATA_DIR    <- paste0(PROJECT_DIR,"data/")
@@ -55,31 +58,35 @@ pg_melt <- melt(pg_us_filt %>% select(tconst,titleType,primaryTitle,startYear,ce
                                       sex,violence,profanity,drugs,intense),
                 id.vars = c("tconst","titleType","primaryTitle","startYear","certificate")) %>% rename(guide=variable,level=value) 
 
-pg_melt$level <- factor(pg_melt$level,levels=c("None","Mild","Moderate","Severe"))
-pg_melt$rating <- as.numeric(pg_melt$level)
+pg_melt$level  <- factor(pg_melt$level,levels=c("None","Mild","Moderate","Severe"))
+pg_melt$rating <- factor(pg_melt$certificate,levels=c("G","PG","PG-13","R"))
+pg_melt$score  <- as.numeric(pg_melt$level)  # Numeric value to parental guide 
+pg_melt$score  <- pg_melt$score-1             # Adjust to scale 0-3 instead of 1-4
 
-png(paste0(PLOT_DIR,"pg_us_all.png"),width=800,height=800)
-ggplot(pg_melt, aes(x=startYear, y=rating, color=guide)) + 
+#png(paste0(PLOT_DIR,"pg_us_all.png"),width=800,height=800)
+svg(paste0(PLOT_DIR,"pg_us_all.svg"))
+ggplot(pg_melt, aes(x=startYear, y=score, color=guide)) + 
   stat_summary(fun=mean, geom="line", size=1)+
   ggtitle("Parental Guides by Year and Guide") +
   theme(plot.title=element_text(hjust=0.5))+
   xlab("Year")+ylab("Level") + labs(color="Type")+ 
-  scale_y_continuous(limits=c(1,4),breaks=c(1,2,3,4),labels=c("None","Mild","Moderate","Severe"))+
+  scale_y_continuous(limits=c(0,3),breaks=c(0,1,2,3),labels=c("None","Mild","Moderate","Severe"))+
   theme(legend.position = "bottom")
 dev.off()
 
 # US Ratings by Guide as a loop
-for (g in guides){
-  single_guide <- pg_melt %>% filter(guide==g)
-  print(paste0("Plotting ",g," to ","pg_us_",g,".png"))
-  png(paste0(PLOT_DIR,"pg_us_",g,".png"),width=800,height=800)
-  print(ggplot(single_guide , aes(x=startYear, y=rating, color=certificate)) + 
+for (gde in guides){
+  single_guide <- pg_melt %>% filter(guide==gde)
+  print(paste0("Plotting ",gde," to ","pg_us_",gde,".png"))
+  #png(paste0(PLOT_DIR,"pg_us_",gde,".png"),width=800,height=800)
+  svg(paste0(PLOT_DIR,"pg_us_",gde,".svg"))
+  print(ggplot(single_guide , aes(x=startYear, y=score, color=certificate)) + 
     stat_summary(fun=mean, geom="line", size=1)+
-    ggtitle(paste(str_to_title(g),"Rating by Year and Certificate")) +
+    ggtitle(paste(str_to_title(gde),"Rating by Year and Certificate")) +
     theme(plot.title=element_text(hjust=0.5))+
     xlab("Year")+ylab("Level") + labs(color="MPAA Certificate")+ 
-    scale_y_continuous(limits=c(1,4),breaks=c(1,2,3,4),labels=c("None","Mild","Moderate","Severe"))+
-    theme(legend.position = "bottom"))
+      scale_y_continuous(limits=c(0,3),breaks=c(0,1,2,3),labels=c("None","Mild","Moderate","Severe"))+
+      theme(legend.position = "bottom"))
   dev.off()
 }
 
@@ -87,15 +94,58 @@ for (g in guides){
 for (cert in mpaa_sub){
   single_cert <- pg_melt %>% filter(certificate==cert)
   print(paste0("Plotting ",cert," to ","pg_us_",cert,".png"))
-  png(paste0(PLOT_DIR,"pg_us_",cert,".png"),width=800,height=800)
-  print(ggplot(single_cert , aes(x=startYear, y=rating, color=guide)) + 
+  #png(paste0(PLOT_DIR,"pg_us_",cert,".png"),width=800,height=800)
+  svg(paste0(PLOT_DIR,"pg_us_",cert,".svg"))
+  print(ggplot(single_cert , aes(x=startYear, y=score, color=guide)) + 
     stat_summary(fun=mean, geom="line", size=1)+
     ggtitle(paste(cert,"Rating by Year and Parental Guide")) +
     theme(plot.title=element_text(hjust=0.5))+
     xlab("Year")+ylab("Level") + labs(color="Parental Guide")+ 
-    scale_y_continuous(limits=c(1,4),breaks=c(1,2,3,4),labels=c("None","Mild","Moderate","Severe"))+
-    theme(legend.position = "bottom"))
+      scale_y_continuous(limits=c(0,3),breaks=c(0,1,2,3),labels=c("None","Mild","Moderate","Severe"))+
+      theme(legend.position = "bottom"))
   dev.off()
 }
 
+# Stacked Area Cert by Guide with ave line
+for (cert in mpaa_sub){
+  for (gde in guides){
+    single_gc <- pg_melt %>% filter(guide==gde) %>% filter(certificate==cert) %>% 
+      group_by(startYear, level) %>% summarise(titles=n()) %>% 
+      mutate(title_pct = 3*titles/sum(titles), 
+             sum_titles = sum(titles),
+             sum_pct = sum(title_pct) )
+    
+    sum_titles <- single_gc %>% select(startYear,sum_titles) %>% unique()
+    
+    single_null <- data.frame(startYear=rep(seq(1980,2022),4),
+                              level=rep(c("None","Mild","Moderate","Severe"),43),
+                              titles=rep(0,172),
+                              title_pct=rep(0,172),
+                              sum_pct=rep(3,172))
+    single_null <- single_null %>% left_join(sum_titles)
 
+    single_null$level <- factor(single_null$level,levels=c("None","Mild","Moderate","Severe"))
+    
+    single_miss <- single_null %>% anti_join(single_gc %>% select(startYear,level) %>% unique())
+
+    single_gc <- rbind(single_gc,single_miss) %>% arrange(startYear,level) %>% unique()
+    
+    single_gc_ave <- pg_melt %>% filter(guide==gde) %>% filter(certificate==cert) %>% 
+      group_by(startYear) %>% summarise(ave_score = mean(score))
+
+    #Line Superimposed on Area
+    print(paste0("Plotting Certificate ",cert," and Guide ",gde," to ","pg_us_",cert,"_",gde,".png"))
+    #png(paste0(PLOT_DIR,"pg_us_",cert,"_",gde,".png"),width=800,height=800)
+    svg(paste0(PLOT_DIR,"pg_us_",cert,"_",gde,".svg"))
+    print(ggplot() +
+      geom_area(data=single_gc , aes(x=startYear, y=title_pct,  fill=fct_rev(level)), alpha=0.6 , size=0.1) +
+      geom_line(data=single_gc_ave , aes(x=startYear, y=ave_score)) +
+        ggtitle(paste("MPAA Certificate:",cert,"Parental Guide:",str_to_title(gde),"Rating by Year")) +
+        theme(plot.title=element_text(hjust=0.5))+
+        xlab("Year")+ylab("Level") + labs(color="Parental Guide")+ 
+        guides(fill=guide_legend(title="Level:")) +
+        scale_y_continuous(limits=c(0,3),breaks=c(0,1,2,3),labels=c("None","Mild","Moderate","Severe")) +
+        theme(legend.position = "bottom"))
+    dev.off()
+  }
+}
